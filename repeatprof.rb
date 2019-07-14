5106679b337f41d3b06e6666b8b8bb4529b369bb
Sherif <negm@rochester.edu>
2019-07-14 18:47:10 -0400

    Update repeatprof.rb

--- repeatprof.rb
+++ repeatprof.rb
@@ -26,6 +26,7 @@ class Repeatprof < Formula
     libexec.install("multi_Poly_maker.R")
     libexec.install("pileup_basecount_sink.py")
     libexec.install("user_supplied_maker.R")
+    system "brew", "link", "--overwrite", "r"
     mv "R_packages", prefix
   end
     
