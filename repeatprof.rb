1f72240525133c1ad375b003538e0f260138fd38
Sherif <negm@rochester.edu>
2019-07-29 05:07:34 -0400

    Update repeatprof.rb

--- repeatprof.rb
+++ repeatprof.rb
@@ -3,7 +3,7 @@ class Repeatprof < Formula
   homepage "https://github.com/johnssproul/RepeatProfiler/"
   url "https://github.com/johnssproul/RepeatProfiler/releases/download/0.91/Repeat-Profiler-v0.91.tar.gz"
   version "0.91"
-  sha256 "4e933f60dec58c30433691ece9c4cb0e0d9062973d6b07cf9dbe98bef0836508"
+  sha256 "9fe958a760f7006374b189e776ca858b8049c61559d8f959e2542e0f01770155"
   depends_on "bowtie2"
   depends_on "python2"
   depends_on "r"
