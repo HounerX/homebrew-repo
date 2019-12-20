1207b7d471974bb03df3673e4c902a2216bc3a38
Sherif <negm@rochester.edu>
2019-12-20 14:13:17 -0500

    Update repeatprof.rb

--- repeatprof.rb
+++ repeatprof.rb
@@ -1,8 +1,8 @@
 class Repeatprof < Formula
   desc 	"RepeatProfiler: A tool for generating, visualizing, and comparing repetitive DNA profiles"
   homepage "https://github.com/johnssproul/RepeatProfiler/"
-  url "https://github.com/johnssproul/RepeatProfiler/releases/download/0.94/Repeat-Profiler-v0.94.tar.gz"
-  version "0.94"
+  url "https://github.com/johnssproul/RepeatProfiler/releases/download/0.95/Repeat-Profiler-v0.95.tar.gz"
+  version "0.95"
   sha256 "cced3b4730ed31025a920884769481f72c9cdf08cf6d215c69ca6c5e33f03963"
   depends_on "bowtie2"
   depends_on "python2"
