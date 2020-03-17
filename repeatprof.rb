00db234aa4b80e904e1198aecba7ae529424a04a
Sherif <negm@rochester.edu>
2020-03-16 20:19:08 -0400

    v0.96

--- repeatprof.rb
+++ repeatprof.rb
@@ -1,9 +1,9 @@
 class Repeatprof < Formula
   desc 	"RepeatProfiler: A tool for generating, visualizing, and comparing repetitive DNA profiles"
   homepage "https://github.com/johnssproul/RepeatProfiler/"
-  url "https://github.com/johnssproul/RepeatProfiler/releases/download/0.95/Repeat-Profiler-v0.95.tar.gz"
-  version "0.95"
-  sha256 "2f2ae450406166515619f56bf8ce5c38883deef72df25c31ea4509333a2bb4a1"
+  url "https://github.com/johnssproul/RepeatProfiler/releases/download/0.96/Repeat-Profiler-v0.96.tar.gz"
+  version "0.96"
+  sha256 "0a70b0120f8bab2ad266cc25e26d12958934df1164836a7fe0d79ad60d014c9a"
   depends_on "bowtie2"
   depends_on "python2"
   depends_on "r"
@@ -13,23 +13,22 @@ class Repeatprof < Formula
     mkdir_p buildpath/"R_packages"
     system "Rscript", "-e", "install.packages(c('ggplot2','reshape2','scales','ggpubr'),dep=TRUE,repos='https://cran.rstudio.com',lib='R_packages')"
     bin.install("repeatprof")
-    libexec.install("Readmegen.sh")
-    libexec.install("map_mpileup.sh")
-    libexec.install("Fasta_splitter.sh")
-    libexec.install("The_depth_analyser.R")
-    libexec.install("RP_4.0.R")
-    libexec.install("polymorphism_2.0.R")
-    libexec.install("fraction_bases.R")
-    libexec.install("Corr_test.R")
-    libexec.install("All_RP_graphs_reference.R")
-    libexec.install("All_RP_graphs.R")
-    libexec.install("multi_Poly_maker.R")
-    libexec.install("pileup_basecount_sink.py")
-    libexec.install("user_supplied_maker.R")
-    libexec.install("all_corr.R")
+    libexec.install("readme_gen.sh")
+    libexec.install("map_reads.sh")
+    libexec.install("fasta_splitter.sh")
     libexec.install("singlecopy.sh")
+    libexec.install("all_corr.R")
+    libexec.install("mk_profiles_ref.R")
+    libexec.install("var_plots.R")
+    libexec.install("multi_var_plots.R")
+    libexec.install("mk_profiles_all.R")
+    libexec.install("mk_profiles.R")
+    libexec.install("mk_profiles_single.R")
+    libexec.install("depth_analyser.R")
+    libexec.install("pileup_basecount.py")
+    libexec.install("encode_var.R")
+    libexec.install("user_groups_maker.R")
     libexec.install("single_copy_calculator.R")
-    libexec.install("singlecopy_map_mpileup.sh")
     mv "R_packages", prefix
   end
     
